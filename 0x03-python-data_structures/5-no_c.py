5c281fbb2e981343e2fd9eb85c8549ff7bfc2639
Herman100 <hermankwamebour30@gmail.com>
2023-05-14 17:48:37 +0000

    code file

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    if my_list is None:
+        return None
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
