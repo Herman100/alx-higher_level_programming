17f902905903e941fa06c81137ab73c7ecb7eae2
Herman100 <hermankwamebour30@gmail.com>
2023-05-30 13:34:56 +0000

    Write an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        width: The width of the square.
+        height: The height of the square.
+    """
+
+    pass
