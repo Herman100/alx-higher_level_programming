8b9b9d7d132b452b12c8d46a7a4734f287f2768d
Herman100 <hermankwamebour30@gmail.com>
2023-06-19 23:20:11 +0000

    test file improved

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,6 +19,15 @@ class TestBase(unittest.TestCase):
         b4 = Base()
         self.assertEqual(b4.id, 3)
 
+    def test_to_json_string(self):
+        """Test to_json_string method"""
+        d1 = {'id': 1, 'width': 10, 'height': 2, 'x': 1, 'y': 9}
+        d2 = {'id': 2, 'width': 5, 'height': 5, 'x': 7, 'y': 8}
+        json_str = Base.to_json_string([d1, d2])
+        expect_str = ('[{"id": 1, "width": 10, "height": 2, "x": 1, "y": 9}, '
+                      '{"id": 2, "width": 5, "height": 5, "x": 7, "y": 8}]')
+        self.assertEqual(json_str, expect_str)
+
 
 if __name__ == '__main__':
     unittest.main()
