b46bd4674373873a9373c70b4a7afb81c6e4b403
Herman100 <hermankwamebour30@gmail.com>
2023-06-20 01:30:50 +0000

    test file update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,16 +34,46 @@ class TestBase(unittest.TestCase):
                       '{"id": 2, "width": 5, "height": 5, "x": 7, "y": 8}]')
         self.assertEqual(json_str, expect_str)
 
+    def test_from_json_string(self):
+        """Test from_json_string method"""
+        json_str = ('[{"id": 1, "width": 10, "height": 2, "x": 1, "y": 9}, '
+                    '{"id": 2, "width": 5, "height": 5, "x":7 ,"y" :8}]')
+        list_of_dicts = Base.from_json_string(json_str)
+        self.assertEqual(list_of_dicts,
+                         [{'id': 1,
+                           'width': 10,
+                           'height': 2,
+                           'x': 1,
+                           'y': 9},
+                          {'id': 2,
+                           'width': 5,
+                           'height': 5,
+                           'x': 7,
+                           'y': 8}])
+
+    def test_create(self):
+        """Test create method"""
+        r1 = Rectangle(3, 4, 5, 6, 7)
+        r1_dict = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dict)
+        self.assertEqual(r1.__str__(), r2.__str__())
+        self.assertIsNot(r1, r2)
+
+    def test_load_from_file(self):
+        """Test load_from_file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        list_rectangles_output = Rectangle.load_from_file()
+
+        self.assertEqual(str(list_rectangles_output[0]), str(r1))
+
     def test_save_to_file(self):
         """Test save_to_file method"""
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         Rectangle.save_to_file([r1, r2])
-        with open("Rectangle.json", "r") as f:
-            self.assertEqual(f.read(), '[{"id": 1, "width": 10, '
-                                       '"height": 7, "x": 2, "y": 8}, '
-                                       '{"id": 2, "width": 2, '
-                                       '"height": 4, "x": 0, "y": 0}]')
 
 
 if __name__ == '__main__':
