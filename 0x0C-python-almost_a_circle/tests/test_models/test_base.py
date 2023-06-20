f6844d9f5248f1510e82a700e37f2b8fe189d4e2
Herman100 <hermankwamebour30@gmail.com>
2023-06-20 00:08:46 +0000

    test base file updated

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,15 @@
 import unittest
+import os
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
     """Test cases for Base class"""
+
+    def setUp(self):
+        """Reset __nb_objects before each test"""
+        Base._Base__nb_objects = 0
 
     def test_id(self):
         """Test id attribute"""
@@ -27,6 +33,17 @@ class TestBase(unittest.TestCase):
         expect_str = ('[{"id": 1, "width": 10, "height": 2, "x": 1, "y": 9}, '
                       '{"id": 2, "width": 5, "height": 5, "x": 7, "y": 8}]')
         self.assertEqual(json_str, expect_str)
+
+    def test_save_to_file(self):
+        """Test save_to_file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 1, "width": 10, '
+                                       '"height": 7, "x": 2, "y": 8}, '
+                                       '{"id": 2, "width": 2, '
+                                       '"height": 4, "x": 0, "y": 0}]')
 
 
 if __name__ == '__main__':
