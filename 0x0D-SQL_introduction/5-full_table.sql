3eb4a17eff992343972579491b83ac44572e4199
Herman100 <hermankwamebour30@gmail.com>
2023-07-18 23:04:15 +0000

    full table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Print the full description of the first_table table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
