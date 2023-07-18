cdef1a55b95248e2a8db228c0b3d0c75b35ceeef
Herman100 <hermankwamebour30@gmail.com>
2023-07-18 23:12:28 +0000

    full table show

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,14 +1,2 @@
--- Script to print the full description of the table first_table
--- from the database hbtn_0c_0 in the MySQL server
-
--- Get the database name from the command line argument
-database_name=$1
-
--- Connect to the database
-mysql -u root -p $database_name
-
--- Get the create table statement for the first_table table
-create_table_statement=`SHOW CREATE TABLE first_table`;
-
--- Print the create table statement
-echo "$create_table_statement"
+-- Print the full description of the first_table table
+SHOW CREATE TABLE first_table;
