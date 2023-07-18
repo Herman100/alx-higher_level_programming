a48542f281dadf47a85d1076a75468840c323f1f
Herman100 <hermankwamebour30@gmail.com>
2023-07-18 23:10:24 +0000

    full table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,14 @@
--- Retrieve the full description of the first_table table
-SELECT column_name, data_type, character_maximum_length, is_nullable
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+-- Script to print the full description of the table first_table
+-- from the database hbtn_0c_0 in the MySQL server
+
+-- Get the database name from the command line argument
+database_name=$1
+
+-- Connect to the database
+mysql -u root -p $database_name
+
+-- Get the create table statement for the first_table table
+create_table_statement=`SHOW CREATE TABLE first_table`;
+
+-- Print the create table statement
+echo "$create_table_statement"
