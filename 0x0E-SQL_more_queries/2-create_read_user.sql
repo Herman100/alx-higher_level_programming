fa27e572a60bb2cf632e52e4cc1dcece84e785b1
Herman100 <hermankwamebour30@gmail.com>
2023-07-19 23:06:49 +0000

    sudo updating user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Create user if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2:!q:'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
