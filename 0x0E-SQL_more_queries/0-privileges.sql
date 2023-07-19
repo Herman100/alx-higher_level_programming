f0fe310c5c78657c7f7aa2ce71abb8e8ee88228b
Herman100 <hermankwamebour30@gmail.com>
2023-07-19 22:08:16 +0000

    lists all privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
-SELECT User, Host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Reload_priv, Shutdown_priv, Process_priv, File_priv, Grant_priv, References_priv, Index_priv, Alter_priv
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2') AND Host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
