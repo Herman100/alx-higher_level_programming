7b6139ee0374819b029b172f37265a6d804a8e94
Herman100 <hermankwamebour30@gmail.com>
2023-07-19 22:55:30 +0000

    creating user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Create user if not exists
+-- script that creates the MySQL server user user_0d_1 with all privileges and sets
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
