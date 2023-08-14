a0b23545115981c377f16983a77f1189ff8082bc
Herman100 <hermankwamebour30@gmail.com>
2023-08-14 21:19:57 +0000

    interger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const firstArg = process.argv[2];
 
 if (isNaN(firstArg)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number:', parseInt(firstArg));
+  console.log('My number:', parseInt(firstArg));
 }
