bc81e4f093dc9ae9efd378b0a861c7e91a18bbeb
Herman100 <hermankwamebour30@gmail.com>
2023-08-14 22:25:41 +0000

    incrementing

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,10 +4,12 @@ const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 myObject.incr = function () {
   myObject.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
