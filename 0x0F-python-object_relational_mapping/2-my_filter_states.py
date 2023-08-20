5a065f071e0b1d045af70ba0a48c939f662906c8
Herman100 <hermankwamebour30@gmail.com>
2023-08-20 21:43:21 +0000

    filter states distinct

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
+"""Lists all states with a name matching the argument from the database
+hbtn_0e_0_usa.
 """
 
 import MySQLdb
@@ -8,13 +8,22 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                (argv[4],))
-    rows = cur.fetchall()
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
+
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
